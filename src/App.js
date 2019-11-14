9424565c2c8e03653ab035e1f719e7db54b2ba91
Brian Hague <alphaseinor@gmail.com>
2019-11-14 02:09:43 -0600

    added removeItem for cart

--- src/App.js
+++ src/App.js
@@ -20,18 +20,19 @@ function App() {
 		setCart(cart => [...cart, item])
 	};
 
+	const removeItem = removeThis => {
+			setCart(cart => cart.filter(item=>item.id!==removeThis))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<CartContext.Provider value={{cart}}>
+				<CartContext.Provider value={{cart, removeItem}}>
 					<Navigation cart={cart} />
 
 					{/* Routes */}
 					<Route exact path='/' component={Products} />
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/>
+					<Route path="/cart" component={ShoppingCart}/>
 				</ CartContext.Provider>
 			</ ProductContext.Provider>
 		</div>
