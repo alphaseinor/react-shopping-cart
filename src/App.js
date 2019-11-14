dbad819aaec6cae57e29d8aa8e8220b635bf177d
Brian Hague <alphaseinor@gmail.com>
2019-11-14 01:46:39 -0600

    refactored ShoppingCart.js for CartContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+//Base imports
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
-import {ProductContext} from './contexts/ProductContext'
 import data from './data';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+// Contexts
+import {ProductContext} from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -19,14 +23,16 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={{cart}}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path='/' component={Products} />
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					{/* Routes */}
+					<Route exact path='/' component={Products} />
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</ CartContext.Provider>
 			</ ProductContext.Provider>
 		</div>
 	);
