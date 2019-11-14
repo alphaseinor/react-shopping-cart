c8c0a1ae8d8dd6e0c10e046d307f83e7a40e97a9
Brian Hague <alphaseinor@gmail.com>
2019-11-14 01:26:48 -0600

    refactored App.js for context provider and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import {ProductContext} from './contexts/ProductContext'
 import data from './data';
 
 // Components
@@ -17,24 +18,16 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				{/* Routes */}
+				<Route exact path='/' component={Products} />
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</ ProductContext.Provider>
 		</div>
 	);
 }
